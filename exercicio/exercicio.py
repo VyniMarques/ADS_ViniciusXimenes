0bf9ceafb0740a1f67ae99ad844fb2def9f19566
VyniMarques <89865087+VyniMarques@users.noreply.github.com>
2022-10-17 21:24:40 -0300

    git

--- exercicio/exercicio.py
+++
@@ -1,9 +0,0 @@
-from math import pow
-
-val = float(input("Entre com um número: "))
-res = pow(val, 3)
-
-if res > 100:
-    print(f'{res} é maior que 100')
-else:
-    print(f'{res} é menor que 100')
